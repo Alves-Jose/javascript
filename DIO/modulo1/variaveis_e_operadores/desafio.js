323ad0687bf9f1b2ba81fd0640f6c463bde8a409
Alves-Jose <josemarcos8133@gmail.com>
2023-09-17 16:39:51 -0300

    updates

---
+++ DIO/modulo1/variaveis_e_operadores/desafio.js
@@ -0,0 +1,12 @@
+//faça um programa para calcular o valor de uma viagem.
+//você terá 3 variáveis. Sendo elas:
+//Preço do combustível;
+//Gasto médiio de combustível do carro por km;
+//distância de km da viagem;
+//imprima no console o valor que será gasto para realizar esta viagem;
+
+let preco_combustivel = 5.14;
+let gasto_comb = 5;
+let dist = 200;
+
+valor_km = 

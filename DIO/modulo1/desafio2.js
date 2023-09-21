6e23986af9084c27510fdf51e04165a46f298ed0
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2023-09-21 14:21:21 -0300

    finish

--- DIO/modulo1/desafio2.js
+++ DIO/modulo1/desafio2.js
@@ -1,17 +1,26 @@
 //faça um programa para calcular o valor de uma viagem.
 
-//você terá 3 variáveis. Sendo elas:
+//você terá 5 variáveis. Sendo elas:
 //preço álcool 
 //preço gasolina
+//tipo de combustivel que está no carro;
 //Gasto médiio de combustível do carro por km;
 //distância de km da viagem;
 //imprima no console o valor que será gasto para realizar esta viagem;
 
-const preco_etanol= 5.14;
-const preco_gasolina = 6.40;
-const gasto_comb = 5;
-const dist = 200;
+const preco_etanol= 5.79;
+const preco_gasolina = 6.66;
+const tipoCombustivel = 'Gasolina'; 
+const gasto_comb = 10;
+const distanciakm = 100;
 
-const litros_consumidos = dist / gasto_comb;
-const total_gasto = litros_consumidos * preco_combustivel;
-console.log(total_gasto.toFixed(2));
+const litros_consumidos = distanciakm / gasto_comb;
+
+if (tipoCombustivel === 'Etanol'){
+    const total_gasto = litros_consumidos * preco_etanol;
+    console.log(total_gasto.toFixed(2));
+
+} else {
+    const total_gasto = litros_consumidos * preco_gasolina;
+    console.log(total_gasto.toFixed(2));
+}

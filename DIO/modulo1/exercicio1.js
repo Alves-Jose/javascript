778f7caac61ae77ad579f7bf173942478d668d49
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2023-09-21 15:06:24 -0300

    Create exercicio1.js

---
+++ DIO/modulo1/exercicio1.js
@@ -0,0 +1,26 @@
+/* Faça um algoritmo que dado as 3 notas tiradas por um aluno em um semestre calcule e imprima a sua média
+e a sua classificação conforme a tabela abaixo.
+
+media = (nota 1 + nota 2 + nota 3)) / 3;
+
+Classificação: 
+
+- Média menor que 5, reprovação;
+- Média entre 5 e 7, recuperação;
+- Média acima de 7, passou de semestre;
+*/
+
+const nota1 = 10.0
+const nota2 = 10.0 
+const nota3 = 10.0
+
+const media = (nota1 + nota2 + nota3) / 3;
+console.log(media.toFixed(2));
+
+    if (media < 5.0){
+        console.log('Reprovação!');
+    } else if (media >= 5 && media <=7 ) {
+        console.log('Recuperação');
+    } else{
+        console.log('Passou de semestre!');
+    }

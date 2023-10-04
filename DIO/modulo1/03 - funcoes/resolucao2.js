39ca12dbc0dda01a7a116ffc4c816af0fd0e3325
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2023-10-04 11:25:05 -0300

    update

---
+++ DIO/modulo1/03 - funcoes/resolucao2.js
@@ -0,0 +1,9 @@
+function verificaridade(idade){
+    if(idade >= 18){
+        console.log('Você é maior de idade');
+    } else{
+        console.log('Você é menor de idade');
+    }
+}
+
+verificaridade(11);

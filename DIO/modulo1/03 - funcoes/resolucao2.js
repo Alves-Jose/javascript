ac584c90b757b47ca55ad8a7c501da69b30619f1
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2023-10-04 11:27:42 -0300

    Update resolucao2.js

--- DIO/modulo1/03 - funcoes/resolucao2.js
+++ DIO/modulo1/03 - funcoes/resolucao2.js
@@ -1,9 +1,15 @@
-function verificaridade(idade){
-    if(idade >= 18){
-        console.log('Você é maior de idade');
-    } else{
-        console.log('Você é menor de idade');
-    }
+function EscrevaMeuNome(nome) {
+  console.log("Seu nome é: " + nome);
+}
+
+EscrevaMeuNome("Marcos");
+
+function verificaridade(idade) {
+  if (idade >= 18) {
+    console.log("Você é maior de idade");
+  } else {
+    console.log("Você é menor de idade");
+  }
 }
 
 verificaridade(11);

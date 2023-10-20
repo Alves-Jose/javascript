8bc933db7d8df88de3a26f856e3bc148b5cd7b23
Alves-Jose <josemarcos8133@gmail.com>
2023-10-20 02:10:21 -0300

    Update teste.js

--- DIO/modulo1/03 - funcoes/teste.js
+++ DIO/modulo1/03 - funcoes/teste.js
@@ -22,3 +22,36 @@ const resp = parimpar(11);
 
 console.log(resp);*/
 
+/*function maiornumero(n1, n2){
+    if (n1 > n2)
+    return 'N1 é maior';
+    else  
+    return 'N2 é maior';
+}
+
+const resp = maiornumero(2,7);
+
+console.log(resp);*/
+
+/*function positivoNegativo(num){
+    if(num > 0)
+    return 'O número é positivo';
+    else if(num < 0)
+    return 'O número e negativo';
+    else
+    return 'O número é nulo';
+}
+
+const resp = positivoNegativo(1);
+
+console.log(resp);*/
+
+/*function parImpar(numero){
+    if(numero % 2 == 0 && numero > 0)
+    return 'true';
+    else
+    return 'false';
+}
+
+console.log(parImpar(2));*/
+

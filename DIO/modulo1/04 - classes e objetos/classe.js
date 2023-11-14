216a6ad75c1ab82e9491fb61cd791d97d7366934
Alves-Jose <josemarcos8133@gmail.com>
2023-11-14 15:45:03 -0300

    Update classe.js

--- DIO/modulo1/04 - classes e objetos/classe.js
+++ DIO/modulo1/04 - classes e objetos/classe.js
@@ -4,10 +4,20 @@ class Pessoa {
     idade;
 
     descrever() {
-        console.log('Meu nome é ${this.nome} e minha idade é ${this.idade}');
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
 }
 
 const jose = new Pessoa();
+jose.nome = 'Jose Marcos P Alves';
+jose.idade = 23;
 
+const beatriz = new Pessoa();
+beatriz.nome = 'Beatriz';
+beatriz.idade = '22';
+
+console.log(beatriz);
 console.log(jose);
+
+beatriz.descrever();
+jose.descrever();

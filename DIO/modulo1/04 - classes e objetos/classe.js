503b1d9adfd35f691b13e6b70680a891813c5f36
Alves-Jose <josemarcos8133@gmail.com>
2023-11-14 15:35:48 -0300

    Create classe.js

---
+++ DIO/modulo1/04 - classes e objetos/classe.js
@@ -0,0 +1,13 @@
+
+class Pessoa {
+    nome;
+    idade;
+
+    descrever() {
+        console.log('Meu nome é ${this.nome} e minha idade é ${this.idade}');
+    }
+}
+
+const jose = new Pessoa();
+
+console.log(jose);

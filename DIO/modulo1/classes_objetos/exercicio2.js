323cd331a03c95fa42e66edb0b97c73063cda8e6
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2024-01-05 23:32:53 -0300

    Create exercicio2.js

---
+++ DIO/modulo1/classes_objetos/exercicio2.js
@@ -0,0 +1,13 @@
+class Person{
+    name;
+    weight;
+    height;
+
+    constructor(name, weight, height){
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    imc(name,weight)
+}

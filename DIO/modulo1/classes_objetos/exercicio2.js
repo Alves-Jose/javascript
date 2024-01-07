2420c4a2a6aecd088512f68436334117258084db
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2024-01-07 00:17:48 -0300

    Update exercicio2.js

--- DIO/modulo1/classes_objetos/exercicio2.js
+++ DIO/modulo1/classes_objetos/exercicio2.js
@@ -1,19 +1,32 @@
-class Person{
-    name;
-    weight;
-    height;
+class Person {
+  name;
+  weight;
+  height;
 
-    constructor(name, weight, height){
-        this.name = name;
-        this.weight = weight;
-        this.height = height;
-    }
+  constructor(name, weight, height) {
+    this.name = name;
+    this.weight = weight;
+    this.height = height;
+  }
 
-    imc(weight, height){
-      return  this.weight / (this.height * this.height);
+  imc(weight, height) {
+    return this.weight / (this.height * this.height);
+  }
+
+  classIMC() {
+    const imc = this.imc();
+    if (imc < 18.5) {
+      return "Under weight.";
+    } else if (imc >= 18.5 && imc <= 25) {
+      return "Normal weight";
+    } else if (imc >= 30 && imc <= 40) {
+      return "Obese.";
+    } else if (imc > 40) {
+      return "Severe obesity";
     }
+  }
 }
 
-const jose = new Person('José', 70, 1.75);
+const jose = new Person("José", 70, 1.70);
 
-console.log(jose.imc());
+console.log(jose.classIMC());

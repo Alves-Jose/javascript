4b746b28f1ec93dbbbf35cd9ffec28c66459488a
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2024-04-24 11:46:19 -0300

    Update aula12.js

--- Udemy/aula12/aula12.js
+++ Udemy/aula12/aula12.js
@@ -2,9 +2,11 @@ let varA = 'A'; // B
 let varB = 'B'; // C
 let varC = 'C'; // A
 
-const varATemp = varA;
+/*const varATemp = varA;
 varA = varB;
 varB = varC;
-varC = varATemp;
+varC = varATemp;*/
 
-console.log(varA, varB, varATemp);
+[varA, varB, varC] = [varB, varC, varA];
+
+console.log(varA, varB, varC);

7aa43e21e5666b312c7a50b844eaa06232995c80
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2024-04-29 16:30:57 -0300

    Update index.js

---
+++ Udemy/aula14/index.js
@@ -0,0 +1,10 @@
+let num1 = 10.1;
+let num2 = 2.5;
+
+//console.log(num1.toString(2));
+//console.log(num1.toFixed(4));
+//console.log(Number.isInteger(num1));
+//console.log(Number.isInteger(num1));
+let temp = num1 * 5;
+
+console.log(Number.isNaN(temp));

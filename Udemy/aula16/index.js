e496fd92a8874a61fab0409edad9a28695e3e411
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2024-07-02 23:41:39 -0300

    Create index.js

---
+++ Udemy/aula16/index.js
@@ -0,0 +1,9 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+//alunos.unshift('Fábio);; Adiciona no começo
+//alunos.push('Fábio'); Adiciona no final
+//alunos.pop(); Remove o último elemento
+//alunos.shift(); Remove o primeiro elemento
+//alunos.splice(1, 2); Remove o segundo e o terceiro elemento
+//alunos.splice(1, 0, 'Fábio'); Insere o elemento F
+//alunos[alunos.length] = 'José'; Adiciona no Fim

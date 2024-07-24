fd6a1f551876ee1f52b3a44d6cf852911167f2ae
José Marcos <112916915+Alves-Jose@users.noreply.github.com>
2024-07-24 14:56:26 -0300

    ?

--- Udemy/aula14/index.js
+++ Udemy/aula14/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let num1 = 10.1;
 let num2 = 2.5;
 
@@ -8,3 +9,15 @@ let num2 = 2.5;
 let temp = num1 * 5;
 
 console.log(Number.isNaN(temp));
+=======
+let num1 = 10.8; // number
+let num2 = 2.5; //number
+
+//console.log(num1.toString() + num2);
+//num1 = num1.toString();
+//console.log(num1.toString(2));
+//console.log(num1.toFixed(2));
+//console.log(Number.isInteger(num1));
+//let temp = num1 * 5;
+console.log(Number.isNaN(temp));
+>>>>>>> Stashed changes
